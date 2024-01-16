681bfd42b44f0c468c7d073020da44a8e505eee4
josuan_fioroto <142553191+josuanfioroto@users.noreply.github.com>
2024-01-15 22:13:56 -0300

    desenvolvimento_3_joy_class

---
+++ desenvolvimento_3.py
@@ -0,0 +1,25 @@
+for i in range (1,21):
+  if (i == 13):
+    continue
+  else:
+    print(i)
+
+
+
+contador = 1
+
+while (contador <=20):
+  if(contador ==13):
+     contador = contador + 1
+     continue
+  else:
+    print(contador)
+    contador = contador + 1
+
+
+
+for i in range (20,0,-1):
+   if(i ==13):
+      continue
+   else:
+      print(i)
